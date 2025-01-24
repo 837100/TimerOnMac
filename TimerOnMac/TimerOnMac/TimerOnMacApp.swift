9fcec71c7baa0cde10610a8359687403b3d818e3
SEONGGYEONG-NO <shtjdrud0830@naver.com>
2025-01-24 20:44:19 +0900

    메뉴바에서 실행 테스트 코드(주석처리) 추가

--- TimerOnMac/TimerOnMac/TimerOnMacApp.swift
+++ TimerOnMac/TimerOnMac/TimerOnMacApp.swift
@@ -10,8 +10,14 @@ import SwiftUI
 @main
 struct TimerOnMacApp: App {
     var body: some Scene {
+        
         WindowGroup {
             ContentView()
         }
+//        MenuBarExtra("Timer", systemImage: "fitness.timer") {
+//                   ContentView()
+//               }
+//               .menuBarExtraStyle(.window)
+//               .keyboardShortcut("m",modifiers: .command, localization: .automatic)
     }
 }
